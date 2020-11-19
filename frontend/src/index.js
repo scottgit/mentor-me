c6750c0e014f4a0f1191cbca878ead24973cbe9a
Scott Smith <scott.sncsmith@gmail.com>
2020-11-19 09:51:26 -0800

    Create Root component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'redux';
+import configureStore from './store/index.js';
+import Root from './Root';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
