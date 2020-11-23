a19f07be39b4b6e42f59a1506357b82c13e65ad6
Scott Smith <scott.sncsmith@gmail.com>
2020-11-23 14:28:29 -0800

    Design User Homepage

--- frontend/src/components/Pages/UserHome.js
+++ frontend/src/components/Pages/UserHome.js
@@ -3,8 +3,24 @@ import {useSelector} from 'react-redux';
 
 const UserHome = () => {
   const sessionUser = useSelector(state => state.session.user);
+  const name = (sessionUser.goBy || sessionUser.userName);
+  const canBeMentor = sessionUser.mentorDesc !== '';
+  const canBeMentee = sessionUser.menteeDesc !== '';
   return (
-    <h2>Welcome</h2>
+    <>
+      <h2>Welcome {name}</h2>
+      { canBeMentee &&
+        <div className='mentors user_listing'>
+          <h3>Your Mentors</h3>
+          
+        </div>
+      }
+      { canBeMentor &&
+        <div className='mentees user_listing'>
+          <h3>Your Mentees</h3>
+        </div>
+      }
+    </>
   )
 }
 
