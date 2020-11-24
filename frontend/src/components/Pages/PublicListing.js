b98e13afdde8b1ea1298c34da076e774db921371
Scott Smith <scott.sncsmith@gmail.com>
2020-11-23 21:49:09 -0800

    Style public listings

--- frontend/src/components/Pages/PublicListing.js
+++ frontend/src/components/Pages/PublicListing.js
@@ -28,15 +28,33 @@ const PublicListing = () => {
     <>
       <h2>Public {endpoint[0].toUpperCase() + endpoint.slice(1)}</h2>
       {
+        (role === 'mentee' && isMentor &&
+         <p>Here is a listing of people seeking a mentor. Perhaps your area of skill or expertise might meet one or more of these seeker's needs. If you think so, send an Invite to make a connection.</p>
+        )
+        ||
+        (role === 'mentor' && isMentee &&
+        <p>Here is a listing of people who want to mentor others. Perhaps your area of need may be what they have to offer. If you think so, send a Request to make a connection.</p>
+        )
+        ||
+        (role === 'mentee' && !isMentor &&
+        <p>This is a listing of people seeking a mentor. You are not currently able to be a mentor, so if you find someone you think you can help, then add a mentor description to your profile in order to be able to make a connection.</p>
+        )
+        ||
+        (role === 'mentor' && !isMentee &&
+        <p>This is a listing of people who want to mentor others. You are noat currently able to be a mentee, so if you find someone you think can help you grow in an area, then add a mentee description to your profile in order to be able to make a connection.</p>
+        )
+
+      }
+      {
         list &&
-        <ul>
+        <ul className='public-list__list'>
           {list.map(person => {
             const {id, username, goBy, picture, menteeDesc, mentorDesc} = person;
             return (
-              <li key={id}>
-                <h3>{goBy || username}</h3>
-                {person.picture && <img src={picture} alt={role + 'as' + username} />}
-                <p>{
+              <li key={id} className='public-list__list-item'>
+                <h3 className='public-list__name'>{goBy || username}</h3>
+                {person.picture && <img src={picture} alt={role + 'as' + username} className='public-list__img'/>}
+                <p className='public-list__description'>{
                     (role === 'mentee' && menteeDesc) ||
                     (role === 'mentor' && mentorDesc)
                     }
@@ -47,12 +65,12 @@ const PublicListing = () => {
                     || (role === 'mentor' && isMentee)
                   )
                   &&
-                      <button type='button' className='button' value={id}>
+                      <button type='button' className='button public-list__button' value={id}>
                           {connectType} this {role}
                       </button>
                   )
                   || //OR, Current user cannot connect
-                  <p>
+                  <p className='public-list__no-connect-note'>
                     You cannot connect with this {role}.
                     {
                       (role === 'mentee' && 'Add a mentor description to activate yourself as a potential mentor to connect to this mentee.') ||
